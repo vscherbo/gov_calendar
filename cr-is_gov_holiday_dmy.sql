cdf62860493f8e292d2f93816e34a343af17e8f1
Vladimir Scherbo <vscherbo@gmail.com>
2017-06-13 14:41:55 +0300

    обработка символа * для переносов

--- cr-is_gov_holiday_dmy.sql
+++ cr-is_gov_holiday_dmy.sql
@@ -6,14 +6,23 @@ declare
 loc_sql text;
 loc_year_col text := 'Год/Месяц';
 loc_delimiter text := ',';
+loc_asterisk text = '*';
+loc_empty text = '';
 loc_res BOOLEAN;
 BEGIN
-    loc_sql := format('select ARRAY[%s]::text[] <@ regexp_split_to_array(ask_month, %s)::text[] 
+    loc_sql := format('select ARRAY[%s]::text[] <@ regexp_split_to_array(replace(ask_month, %s, %s), %s)::text[] 
     from (select values->>%s as year, values->>%s as ask_month 
     from (select json_array_elements(values::json) as values from gov_holiday_json) j) ym WHERE year=%s;',
-    arg_day, quote_literal(loc_delimiter), quote_literal(loc_year_col), quote_literal(arg_month_name), quote_literal(arg_year));
+    arg_day
+    , quote_literal(loc_asterisk)
+    , quote_literal(loc_empty)
+    , quote_literal(loc_delimiter)
+    , quote_literal(loc_year_col)
+    , quote_literal(arg_month_name)
+    , quote_literal(arg_year));
     -- raise notice 'loc_sql=%', loc_sql;
     EXECUTE loc_sql INTO loc_res;
     RETURN loc_res;
 END  
-$function$
+$function$;
+
